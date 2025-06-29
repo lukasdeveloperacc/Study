64fef5459742e79ca3d6c34c0026962e0becacf6
ChaejinE <joung6517@gmail.com>
2025-06-29 21:09:47 +0900

    Styles APis 1. ScrollView - There are awsome props 2. Dimensions - We can know size of window

--- reactnative/nomadcoder/basic/NomadWeather/app/(tabs)/index.tsx
+++ reactnative/nomadcoder/basic/NomadWeather/app/(tabs)/index.tsx
@@ -1,12 +1,57 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window")
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 2, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <StatusBar style="dark" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>서울</Text>
+      </View>
+      <ScrollView pagingEnabled showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temperature}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, backgroundColor: "tomato"
+  },
+  city: {
+    flex: 1.2, justifyContent: "center", alignItems: "center"
+  },
+  cityName: {
+    fontSize: 68, fontWeight: "500"
+  },
+  weather: {
+    // flex: 3
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center"
+  },
+  temperature: {
+    marginTop: 50,
+    fontSize: 178
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60
+  }
+})
