69c5971b082363c00228f41caeea33d15e01454d
ChaejinE <joung6517@gmail.com>
2025-07-05 22:29:07 +0900

    12. CSS와의 차이점, 비슷한 점 13. Animation

--- reactnative/zerocho/threads/app/(tabs)/(home)/index.tsx
+++ reactnative/zerocho/threads/app/(tabs)/(home)/index.tsx
@@ -1,11 +1,13 @@
+import { BlurView } from "expo-blur";
 import { usePathname, useRouter } from "expo-router";
-import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet, Image } from "react-native";
 import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
     const router = useRouter();
     const pathName = usePathname();
     const insets = useSafeAreaInsets();
+    const isLoggedIn = false;
 
     console.log("path name : ", pathName);
     console.log("insets : ", insets)
@@ -13,34 +15,45 @@ export default function Index() {
     return (
         // <SafeAreaView style={styles.conatiner}>
         <View style={[styles.conatiner, { paddingTop: insets.top, paddingBottom: insets.bottom }]} >
-            <View style={styles.tabContainer}>
-                <View style={styles.tab}>
-                    <TouchableOpacity onPress={() => router.push(`/`)}>
-                        <Text style={{ color: pathName === "/" ? "red" : "black" }}>For you </Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.tab}>
-                    <TouchableOpacity onPress={() => router.push(`/following`)}>
-                        <Text style={{ color: pathName === "/" ? "black" : "red" }}>Following </Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.tab}>
-                    <TouchableOpacity onPress={() => router.push(`/@lotto/post/1`)}>
-                        <Text>게시글1 </Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.tab}>
-                    <TouchableOpacity onPress={() => router.push(`/@lotto/post/2`)}>
-                        <Text>게시글2 </Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.tab}>
-                    <TouchableOpacity onPress={() => router.push(`/@lotto/post/3`)}>
-                        <Text>게시글3 </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            <BlurView style={styles.header} intensity={70}>
+                <Image source={require("../../../assets/images/react-logo.png")}></Image>
+                {isLoggedIn && <TouchableOpacity style={styles.loginButton} onPress={() => router.navigate(`/login`)}>
+                    <Text style={styles.loginButtonText}>로그인</Text>
+                </TouchableOpacity>}
+            </BlurView>
+            {isLoggedIn ??
+                <View style={styles.tabContainer}>
+                    <View style={styles.tab}>
+                        <TouchableOpacity onPress={() => router.push(`/`)}>
+                            <Text style={{ color: pathName === "/" ? "red" : "black" }}>For you </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.tab}>
+                        <TouchableOpacity onPress={() => router.push(`/following`)}>
+                            <Text style={{ color: pathName === "/" ? "black" : "red" }}>Following </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>}
             {/* </SafeAreaView > */}
+            {isLoggedIn ??
+                <>
+                    <View>
+                        <TouchableOpacity onPress={() => router.push(`/@lotto/post/1`)}>
+                            <Text>게시글1 </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <TouchableOpacity onPress={() => router.push(`/@lotto/post/2`)}>
+                            <Text>게시글2 </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <TouchableOpacity onPress={() => router.push(`/@lotto/post/3`)}>
+                            <Text>게시글3 </Text>
+                        </TouchableOpacity>
+                    </View>
+                </>
+            }
         </View>
     );
 }
@@ -53,6 +66,28 @@ const styles = StyleSheet.create({
         flexDirection: "row"
     },
     tab: {
-        flex: 1
+        flex: 1,
+        alignItems: "center"
+    },
+    header: {
+        alignItems: "center"
+    },
+    headerLogo: {
+        width: 42,
+        height: 42
+    },
+    loginButton: {
+        position: "absolute",
+        backgroundColor: "black",
+        right: 20,
+        top: 0,
+        borderWidth: 1,
+        borderColor: "black",
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        borderRadius: 10,
+    },
+    loginButtonText: {
+        color: "white"
     }
 })
--- reactnative/zerocho/threads/app/(tabs)/_layout.tsx
+++ reactnative/zerocho/threads/app/(tabs)/_layout.tsx
@@ -1,7 +1,58 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs, useRouter } from "expo-router";
-import { useState } from "react";
-import { Modal, Text, TouchableOpacity, View } from "react-native";
+import { useRef, useState } from "react";
+import {
+    Modal,
+    View,
+    Text,
+    TouchableOpacity,
+    Animated,
+    Pressable,
+} from "react-native";
+import { type BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
+
+const AnimatedTabBarButton = ({
+    children,
+    onPress,
+    style,
+    ...restProps
+}: BottomTabBarButtonProps) => {
+    const scaleValue = useRef(new Animated.Value(1)).current;
+
+    const handlePressOut = () => {
+        Animated.sequence([
+            Animated.spring(scaleValue, {
+                toValue: 1.2,
+                useNativeDriver: true,
+                speed: 200,
+            }),
+            Animated.spring(scaleValue, {
+                toValue: 1,
+                useNativeDriver: true,
+                speed: 200,
+            }),
+        ]).start();
+    };
+
+    return (
+        <Pressable
+            {...restProps}
+            onPress={onPress}
+            onPressOut={handlePressOut}
+            style={
+                [
+                    { flex: 1, justifyContent: "center", alignItems: "center" },
+                    style,
+                ]}
+            // Disable Android ripple effect
+            android_ripple={{ borderless: false, radius: 0 }}
+        >
+            <Animated.View style={{ transform: [{ scale: scaleValue }] }}>
+                {children}
+            </Animated.View>
+        </Pressable >
+    );
+};
 
 export default function TabLayout() {
     const router = useRouter();
@@ -22,6 +73,7 @@ export default function TabLayout() {
             <Tabs
                 screenOptions={{
                     headerShown: false,
+                    tabBarButton: (props) => <AnimatedTabBarButton {...props} />
                 }}
                 backBehavior="history"
             >
