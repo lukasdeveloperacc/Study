db0fabb2f453954c97e8b925b8a34e51562bcae1
ChaejinE <joung6517@gmail.com>
2025-07-05 21:33:22 +0900

    11. StyleSheet와 react-native-safe-area-context
    - StyleSheet : Auto optimize of design
    - SafeView, SafeAreaView (Custom)

--- reactnative/zerocho/threads/app/(tabs)/(home)/index.tsx
+++ reactnative/zerocho/threads/app/(tabs)/(home)/index.tsx
@@ -1,37 +1,58 @@
 import { usePathname, useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
     const router = useRouter();
     const pathName = usePathname();
+    const insets = useSafeAreaInsets();
 
     console.log("path name : ", pathName);
+    console.log("insets : ", insets)
 
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <View>
-                <TouchableOpacity onPress={() => router.push(`/`)}>
-                    <Text style={{ color: pathName === "/" ? "red" : "black" }}>For you </Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => router.push(`/following`)}>
-                    <Text style={{ color: pathName === "/" ? "black" : "red" }}>Following </Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => router.push(`/@lotto/post/1`)}>
-                    <Text>게시글1 </Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => router.push(`/@lotto/post/2`)}>
-                    <Text>게시글2 </Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => router.push(`/@lotto/post/3`)}>
-                    <Text>게시글3 </Text>
-                </TouchableOpacity>
+        // <SafeAreaView style={styles.conatiner}>
+        <View style={[styles.conatiner, { paddingTop: insets.top, paddingBottom: insets.bottom }]} >
+            <View style={styles.tabContainer}>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/`)}>
+                        <Text style={{ color: pathName === "/" ? "red" : "black" }}>For you </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/following`)}>
+                        <Text style={{ color: pathName === "/" ? "black" : "red" }}>Following </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/@lotto/post/1`)}>
+                        <Text>게시글1 </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/@lotto/post/2`)}>
+                        <Text>게시글2 </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.tab}>
+                    <TouchableOpacity onPress={() => router.push(`/@lotto/post/3`)}>
+                        <Text>게시글3 </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
+            {/* </SafeAreaView > */}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    conatiner: {
+        flex: 1
+    },
+    tabContainer: {
+        flexDirection: "row"
+    },
+    tab: {
+        flex: 1
+    }
+})
