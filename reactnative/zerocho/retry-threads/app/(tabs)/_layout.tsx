54372ebf49966501530749b0d0975a88283ac883
lukasdeveloperacc <lukasdeveloperacc@gmail.com>
2025-07-26 16:11:55 +0900

    Fix : 5번째 강의에서 npm run andorid 시, development build 앱으로 실행되었다. s 를 눌러서 Expo Go로 전환 후 a를 눌러서 andorid를 실행한다.
    6. Expo Router와 _layout, Navigation
    
    - npm run reset-project
      - 알아서 프로젝트가 리셋된다. 수동으로 지울 필요 없다.
    - Expo Router는 Next Router와 구조가 매우 비슷하다고한다.
    - 동적 주소
      - 보통 앱에서는 <app-name>:// 으로 프로토콜을 지정한다. http://와 유사. 예시 상황을 들자면 여러 유저의 이름을 라우팅해야되면 <app-name>:// user1, <app-name>://user2.. 이런식으로 추가를 해야된다. 매우 귀찮을 일이다.
      - [username].tsx
         - 동적 주소 페이지로 대응하면 된다. home.tsx, search.tsx 등의 이름을 제외한 나머지 라우팅 내용의 경우에 라우팅된다.
         - 원래 아무 라우팅 내용이 없으면 +not-found.tsx에 걸리는데, 동적 라우팅이 걸리면 쓸모가 없어진다.
    - 라우팅은 폴더 구조 순으로 라우팅된다.
      - <app-name>//folder-name/.../tsx-name
    - Naviation
      - Stack
         - 순차적으로 렌더링 페이지 히스토리가 쌓여가므로 뒤로가기 시 순차적 back 가능.
         - 메모리에 계속 남아있으니 주의
      - Tab
        - 돋보기, 오픈 채팅 등의 탭들을 위한 navigation
        - (tabs)
          - 소괄호는 주소에 영향을 미치지 않음
      - Drawer
        -  메뉴 누르면 일부 렌더링 되는 navigation (별도 설치 필요)
    - 폴더 만들 때마다 _layout.tsx 만들 수 있다.
      - 해당 폴더 아래의 모든 tsx 페이지는 _layout을 부모로 갖는다.
      - 해당 폴더의 _layout.tsx의 부모는 상위 폴더의 _layout.tsx이다.
    - RootLayout 에서는 google analystics, 권한을 얻는 내용 등을 넣는다.
    - afterLogin의 layout 경우 로그인 후 보여주는 공통된 layout을 넣는다.
    - ()로 폴더를 나누는 이유는 그룹화시키려는 것이다. ()는 라우팅에 전혀 영향이 없음에 유의한다.
    - 모달의 경우 Stack navigator와 Tabs navigator를 섞어서 많이 쓴다. 다른 경우에도 응용하는 경우 많다. 서로 연동해서 사용하는 경우 많다.

--- reactnative/zerocho/retry-threads/app/(tabs)/_layout.tsx
+++ reactnative/zerocho/retry-threads/app/(tabs)/_layout.tsx
@@ -1,45 +1,82 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs, useRouter } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const router = useRouter();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false, // 헤더에 tsx 내용이 나옴 없애버리고 싶으면 false 
+            }}
+        >
+            <Tabs.Screen // Tabs.Screen 으로 탭의 순서를 정할 수 있음 
+                name="index"
+                options={{
+                    tabBarLabel: () => null,
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name="home"
+                            size={24}
+                            color={focused ? "black" : "gray"}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    tabBarLabel: () => null,
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name="search"
+                            size={24}
+                            color={focused ? "black" : "gray"}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="add"
+                listeners={{
+                    tabPress: (e) => {
+                        e.preventDefault(); // add.tsx 뜨는 것 방지 
+                        router.navigate("/modal"); // modal.tsx 를 찾을 것 -> 부모 _layout 체크 -> 모달 처럼뜨는 옵션 확인 후 모달처럼 렌더링!
+                    },
+                }}
+                options={{
+                    tabBarLabel: () => null,
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons name="add" size={24} color={focused ? "black" : "gray"} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="activity"
+                options={{
+                    tabBarLabel: () => null,
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name="heart-outline"
+                            size={24}
+                            color={focused ? "black" : "gray"}
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="[username]"
+                options={{
+                    tabBarLabel: () => null,
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            name="person-outline"
+                            size={24}
+                            color={focused ? "black" : "gray"}
+                        />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
