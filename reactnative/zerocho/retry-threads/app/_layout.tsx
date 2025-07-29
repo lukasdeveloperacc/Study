99512d175b8265daab00791be80b726625d0f7ab
lukasdeveloperacc <lukasdeveloperacc@gmail.com>
2025-07-29 22:26:27 +0900

    26. 스플래시 스크린 커스터마이징(Asset, Constants)
    - 스플래시 스크린 : 초기 앱실행할 때 나오는 스크린
      - 데이터가 준비되는 시간을 벌 수 있다.
    - app.json에서 plugins의 expo-splash-screen 섹션에서 이미지 수정이 가능하다. 하지만 보통 api를 통해 custom 한다.
    
    1. 앱의 기본 스플래시스크린이 렌더링 되어있는 상태
    2. RootLayout이 렌더링될 때 AnimatedAppLoader가 렌더링된다
    3. AnimatedAppLoader
    Asset.loadAsync로 이미지를 미리 로딩한다
    4. 이미지 로딩 후 isSplashReady를 true로 만들어서 AnimatedSplashScreen을 렌더링한다.
    5. AnimatedSplashScreen
    - Animated.View, Animated.Image가 먼저 렌더링되면서 onImageLoaded가 호출된다
    - onImageLoaded에서 데이터들을 준비하고 앱의 기본 스플래시스크린을 제거한다. 그리고 isAppReady를 true로 만든다
    - isAppReady가 true가 되면 Animated.timing이 수행되며 애니메이션이 실행되고, Animated.View 아래에 children(라우터)가 표시된다.
    - 애니메이션 완료 후 isSplashAnimationComplete가 true가 되어 Animated.View가 사라진다.

--- reactnative/zerocho/retry-threads/app/_layout.tsx
+++ reactnative/zerocho/retry-threads/app/_layout.tsx
@@ -1,9 +1,21 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
 import { Stack } from "expo-router";
 import * as SecureStore from "expo-secure-store";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { createContext, useEffect, useState } from "react";
-import { Alert } from "react-native";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { Alert, Animated, StyleSheet, View } from "react-native";
+
+// Instruct SplashScreen not to hide yet, we want to do this manually
+// 기본 splash 스크린이 사라지는 시간과 Animated.Image로 셋팅한 스플래시 스크린이 뜨는 시간에 차이가 발생하면
+// 사용자 경험에 문제가 될 수 있음
+// plugin에 있는 스플래시 스크린이 사라지지 않도록함
+// 수동으로 바로 없애줌
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
 
 export interface User {
   id: string;
@@ -24,8 +36,23 @@ export const AuthContext = createContext<{
   user: null,
 });
 
-export default function RootLayout() {
+function AnimatedAppLoader({
+  children,
+  image,
+}: {
+  children: React.ReactNode;
+  image: number;
+}) {
   const [user, setUser] = useState<User | null>(null);
+  const [isSplashReady, setSplashReady] = useState(false); // react logo 로딩이 끝났는지 확인 
+
+  useEffect(() => {
+    async function prepare() {
+      await Asset.loadAsync(image);
+      setSplashReady(true);
+    }
+    prepare();
+  }, [image]);
 
   const login = () => {
     console.log("login");
@@ -64,25 +91,113 @@ export default function RootLayout() {
     ]);
   };
 
-  const updateUser = (user: User) => {
+  const updateUser = (user: User | null) => {
     setUser(user);
-    AsyncStorage.setItem("user", JSON.stringify(user));
+    if (user) {
+      AsyncStorage.setItem("user", JSON.stringify(user));
+    } else {
+      AsyncStorage.removeItem("user");
+    }
   };
 
+  if (!isSplashReady) {
+    return null;
+  }
+
+  return ( // ContextAPI는 최대한 상위 에있는게 좋다.
+    <AuthContext value={{ user, login, logout, updateUser }}>
+      <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>
+    </AuthContext>
+  );
+}
+
+function AnimatedSplashScreen({
+  children,
+  image,
+}: {
+  children: React.ReactNode;
+  image: number;
+}) {
+  const [isAppReady, setAppReady] = useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
+  const animation = useRef(new Animated.Value(1)).current;
+  const { updateUser } = useContext(AuthContext);
+
   useEffect(() => {
-    AsyncStorage.getItem("user").then((user) => {
-      setUser(user ? JSON.parse(user) : null);
-    });
-    // TODO: validating access token
-  }, []);
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 2000,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  const onImageLoaded = async () => {
+    try {
+      // 데이터 준비
+      await Promise.all([
+        AsyncStorage.getItem("user").then((user) => {
+          updateUser?.(user ? JSON.parse(user) : null);
+        }),
+        // TODO: validating access token
+      ]);
+      await SplashScreen.hideAsync(); // 수동으로 plugins에 있는 스플래시 스크린을 없애줌
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setAppReady(true);
+    }
+  };
+
+  const rotateValue = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
 
   return (
-    <AuthContext value={{ user, login, logout, updateUser }}>
+    <View style={{ flex: 1 }}>
+      {isAppReady && children} {/* 앱이 준비되면 children을 보여주기 */}
+      { /* 앱이 준비되지 않았으면 splash screen을 보여주기 + 애니메이션 끝날 때까지 애니메이션 보여주기  */}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            {
+              ...StyleSheet.absoluteFillObject, // 화면 꽉채우기 
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor:
+                Constants.expoConfig?.splash?.backgroundColor || "#ffffff",
+              opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            source={image}
+            style={{
+              resizeMode: Constants.expoConfig?.splash?.resizeMode || "contain",
+              width: Constants.expoConfig?.splash?.imageWidth || 200,
+              transform: [{ scale: animation }, { rotate: rotateValue }],
+            }}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
+
+export default function RootLayout() {
+  return (
+    <AnimatedAppLoader image={require("../assets/images/react-logo.png")}> {/* require 시 image type은 number */}
       <StatusBar style="auto" animated hidden={false} />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
-    </AuthContext>
+    </AnimatedAppLoader>
   );
 }
