38ea6fbabda8d1013f783c5bed9e641c655bd5cd
reconlabs-luke <luke@reconlabs.ai>
2025-09-15 14:08:58 +0900

    5-1. Firt Flow

--- agents/nomadcoder/CONTENT-PIPELINE-AGENT/main.py
+++ agents/nomadcoder/CONTENT-PIPELINE-AGENT/main.py
@@ -1,6 +1,47 @@
-def main():
-    print("Hello from content-pipeline-agent!")
+from crewai.flow.flow import Flow, listen, start, router, and_, or_
+from pydantic import BaseModel
 
+class MyFirstFolowState(BaseModel):
+    user_id: int = 1
+    is_admin: bool = False
 
-if __name__ == "__main__":
-    main()
+    
+class MyFirstFlow(Flow[MyFirstFolowState]):
+    @start()
+    def first(self):
+        print(self.state.user_id)
+        print("Hello")
+
+    @listen(first)
+    def second(self):
+        self.state.user_id = 2
+        print("World")
+
+    @listen(first)
+    def third(self):
+        print("CrewAI !")
+
+    @listen(and_(second, third))
+    def final(self):
+        print(":)")
+
+    @router(final)
+    def route(self):
+        if self.state.user_id == 2:
+            return 'even' # emit event
+        else:
+            return 'odd'
+
+    @listen("even")
+    def handle_even(self):
+        print("even")
+
+    @listen("odd")
+    def handle_odd(self):
+        print("odd")
+    
+    
+    
+flow = MyFirstFlow()
+# flow.plot()
+flow.kickoff()
