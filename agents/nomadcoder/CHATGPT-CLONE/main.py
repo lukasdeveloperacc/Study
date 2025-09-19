e584ddfeb380a32c11f873ed93c1e00c4737fc26
lukasdeveloperacc <lukasdeveloperacc@gmail.com>
2025-09-19 15:48:31 +0900

    8-0. Chat UI

--- agents/nomadcoder/CHATGPT-CLONE/main.py
+++ agents/nomadcoder/CHATGPT-CLONE/main.py
@@ -1,15 +1,45 @@
+from agents import Agent, Runner ,SQLiteSession
+
+import dotenv
+dotenv.load_dotenv()
+
 import streamlit as st
+import asyncio
 
-if "is_admin" not in st.session_state:
-    st.session_state["is_admin"] = False
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(name="ChatGPTClone", instructions="""You are a helpful assistant.""")
 
-st.header("Hello !")
+agent: Agent = st.session_state["agent"]
 
-name = st.text_input("What is your name ?")
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession("chat-history", "chat-gpt-clone-memory.db")
 
-if name:
-    st.write(f"Hello {name}")
-    st.session_state["is_admin"] = True
+session: SQLiteSession = st.session_state["session"]
 
-print(st.session_state["is_admin"])
+async def run_agent(message):
+    stream = Runner.run_streamed(agent, message, session)
+    async for event in stream.stream_events():
+        if event.type == "raw_response_event":
+            if event.data.type == "response.output_text.delta":
+                with st.chat_message("assistant"):
+                    st.write(event.data.delta)
 
+with st.sidebar:
+    reset = st.button("Reset memory") # if clicked, reset will be True
+    if reset:
+        asyncio.run(session.clear_session())
+
+    st.write(asyncio.run(session.get_items()))
+
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+    with st.chat_message("user"):
+        st.write(prompt)
+    
+    asyncio.run(run_agent(prompt))
+    
+
+    
+
+    
