a9901cf69e23f4f648eb7ffc03eba28604fed6e5
lukasdeveloperacc <lukasdeveloperacc@gmail.com>
2025-10-22 22:11:09 +0900

    17.1 Email graph

---
+++ agents/nomadcoder/TESTING-AGENTS/main.py
@@ -0,0 +1,60 @@
+from typing import Literal, List
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+
+
+class EmailState(TypedDict):
+    email: str
+    category: Literal["spam", "normal", "urgent"]
+    priority_score: int
+    response: str
+
+
+def categorize_email(state: EmailState) -> EmailState:
+    email = state.get("email", "").lower()
+
+    if "urgent" in email or "asap" in email:
+        category = "urgent"
+    elif "offer" in email or "discount" in email:
+        category = "spam"
+    else:
+        category = "normal"
+
+    return {"category": category}
+
+
+def assign_priority(state: EmailState) -> EmailState:
+    scores = {"urgent": 10, "spam": 1, "normal": 5}
+
+    return {"priority_score": scores[state["category"]]}
+
+
+def generate_response(state: EmailState) -> EmailState:
+    responses = {
+        "urgent": "I will answer you as fast as i can",
+        "normal": "I'll get back to you soon",
+        "spam": "Go away!",
+    }
+
+    return {"response": responses[state["category"]]}
+
+
+graph_builder = StateGraph(EmailState)
+
+graph_builder.add_node(categorize_email.__name__, categorize_email)
+graph_builder.add_node(assign_priority.__name__, assign_priority)
+graph_builder.add_node(generate_response.__name__, generate_response)
+
+graph_builder.add_edge(START, categorize_email.__name__)
+graph_builder.add_edge(categorize_email.__name__, assign_priority.__name__)
+graph_builder.add_edge(assign_priority.__name__, generate_response.__name__)
+graph_builder.add_edge(generate_response.__name__, END)
+
+graph = graph_builder.compile()
+
+result = graph.invoke({"email": "i need to talk to you urgently"})
+
+print("Start")
+print(result)
+print("End")
