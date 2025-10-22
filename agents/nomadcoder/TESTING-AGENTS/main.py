0d8cc72769adf0b70086361bae689bfd0b43be34
lukasdeveloperacc <lukasdeveloperacc@gmail.com>
2025-10-22 22:38:48 +0900

    17.3 Testing Nodes

--- agents/nomadcoder/TESTING-AGENTS/main.py
+++ agents/nomadcoder/TESTING-AGENTS/main.py
@@ -1,7 +1,9 @@
-from typing import Literal, List
+from typing import Literal
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import MemorySaver
+
+checkpointer = MemorySaver()
 
 
 class EmailState(TypedDict):
@@ -30,7 +32,7 @@ def assign_priority(state: EmailState) -> EmailState:
     return {"priority_score": scores[state["category"]]}
 
 
-def generate_response(state: EmailState) -> EmailState:
+def draft_response(state: EmailState) -> EmailState:
     responses = {
         "urgent": "I will answer you as fast as i can",
         "normal": "I'll get back to you soon",
@@ -44,11 +46,11 @@ graph_builder = StateGraph(EmailState)
 
 graph_builder.add_node(categorize_email.__name__, categorize_email)
 graph_builder.add_node(assign_priority.__name__, assign_priority)
-graph_builder.add_node(generate_response.__name__, generate_response)
+graph_builder.add_node(draft_response.__name__, draft_response)
 
 graph_builder.add_edge(START, categorize_email.__name__)
 graph_builder.add_edge(categorize_email.__name__, assign_priority.__name__)
-graph_builder.add_edge(assign_priority.__name__, generate_response.__name__)
-graph_builder.add_edge(generate_response.__name__, END)
+graph_builder.add_edge(assign_priority.__name__, draft_response.__name__)
+graph_builder.add_edge(draft_response.__name__, END)
 
-graph = graph_builder.compile()
+graph = graph_builder.compile(checkpointer=checkpointer)
--- agents/nomadcoder/TESTING-AGENTS/test.py
+++ agents/nomadcoder/TESTING-AGENTS/test.py
@@ -12,7 +12,50 @@ import pytest
     ],
 )
 def test_full_graph(email, expected_category, expected_score):
-    result = graph.invoke({"email": email})
+    result = graph.invoke({"email": email}, config={"configurable": {"thread_id": "1"}})
 
     assert result["category"] == expected_category
     assert result["priority_score"] == expected_score
+
+
+# only one node test
+def test_individual_nodes():
+    # categorize_email
+    result = graph.nodes["categorize_email"].invoke(
+        {"email": "Check out this offer"}, config={"configurable": {"thread_id": "1"}}
+    )
+    assert result["category"] == "spam"
+
+    # assign_priority
+    result = graph.nodes["assign_priority"].invoke(
+        {"category": "spam"}, config={"configurable": {"thread_id": "1"}}
+    )
+    assert result["priority_score"] == 1
+
+    # draft_response
+    result = graph.nodes["draft_response"].invoke(
+        {"category": "spam"}, config={"configurable": {"thread_id": "1"}}
+    )
+    assert "Go away!" in result["response"]
+
+
+# Partial graph test
+def test_partial_execution():
+    from main import categorize_email, draft_response
+
+    graph.update_state(
+        config={"configurable": {"thread_id": "1"}},
+        values={
+            "email": "Please check out this offer",
+            "category": "spam",
+        },  # 이미 값이 주어져서 진행된 것처럼 가장할 수 있다.
+        as_node=categorize_email.__name__,  # categorize_email 노드인 척하기
+    )
+
+    result = graph.invoke(
+        None,
+        config={"configurable": {"thread_id": "1"}},
+        interrupt_after=draft_response.__name__,  # 테스트에서 유용한 인터럽트
+    )
+
+    assert result["priority_score"] == 1
