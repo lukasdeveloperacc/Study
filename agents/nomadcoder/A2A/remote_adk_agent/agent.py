80f7874d6063002dd4a75b1e1914701be1c4046b
lukasdeveloperacc <lukasdeveloperacc@gmail.com>
2025-10-23 23:47:47 +0900

    20.2 A2A For dummies

--- agents/nomadcoder/A2A/remote_adk_agent/agent.py
+++ agents/nomadcoder/A2A/remote_adk_agent/agent.py
@@ -6,12 +6,20 @@ from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
 from google.adk.a2a.utils.agent_to_a2a import to_a2a
 
+
+def dummy_tool(hello: str):
+    """Dummy Tool. Helps the agent"""
+    return "world"
+
+
 agent = Agent(
     name="HistoryHelperAgent",
     description="An agent that can help students with their history homework",
     model=LiteLlm("openai/gpt-4o"),
+    tools=[dummy_tool],
     sub_agents=[],
 )
+
 
 app = to_a2a(agent, port=8001)  # create server
 # uv run uvicorn agent:app --port 8001
--- agents/nomadcoder/A2A/user-facing-agent/user_facing_agent/agent.py
+++ agents/nomadcoder/A2A/user-facing-agent/user_facing_agent/agent.py
@@ -5,6 +5,7 @@ load_dotenv()
 from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
 
+
 root_agent = Agent(
     name="StudentHelperAgent",
     description="An agent that can help students with their homework",
